6526e50a7852cae3b7851c33ac129f0b39d5d16e
ryantz <r.tanzyy@gmail.com>
2024-04-01 17:15:11 +0800

    Hellow world in java...gross

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.print("My");
+        System.out.print("Name");
+        System.out.print("Is");
+        System.out.print("Ryan");
+    }
+}
