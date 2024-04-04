23f09f82fa1fdc97c8941e706cd4fce2f1a438f8
ryantz <r.tanzyy@gmail.com>
2024-04-04 15:39:57 +0800

    bank account eg with reassigning instance fields, returns and toString()

---
+++ Bank.java
@@ -0,0 +1,41 @@
+public class Bank{
+    double balance;
+
+    public Bank(double userBalance){
+        balance = userBalance;
+    }
+
+    //reassigning instance fields
+
+    public void withdrawAmount(double withdrawal){
+        double updatedBalance = balance - withdrawal;
+        balance = updatedBalance; //reassign
+        System.out.println("The new amount after withdrawal is: $" + updatedBalance);
+    }
+
+    public void depositAmount(double deposit){
+        double updatedBalance = balance + deposit;
+        balance = updatedBalance; //reassign
+        System.out.println("The new amount after depositing is: $" + updatedBalance);
+    }
+
+    public double savingsInterest(){
+        double totalWithInterest = balance + balance * 0.01;
+        return totalWithInterest; //returns a value to savingsInterest to be displayed
+    }
+
+    //helps return something with more information on the object created rather than just its memory address
+    public String toString(){
+        return "the balance in the account at the end of the transactions is $" + balance; //printing objects
+    }
+    public static void main(String[] args){
+        Bank newAccount = new Bank(300.00);
+        System.out.println("The current balance in the bank is: $" + newAccount.balance);
+        newAccount.withdrawAmount(100);
+        newAccount.depositAmount(400.20);
+        double checkInterest = newAccount.savingsInterest();
+        System.out.println("The total balance including interest is: $" + checkInterest);
+        System.out.println(newAccount); //able to print out more info with the help of toString method
+        //without toString, it will just print out the address the object is stored
+    }
+}
--- Car.java
+++ Car.java
@@ -9,11 +9,21 @@ public class Car{
         age = carAge;
     }
 
-    //method sorta like a function?
+    //method sorta like a function? to automate something that will be done many times
     public void startEngine(){
         //engineMessage cannot be used in main functions too
         String engineMessage = "The car is a " + age + " year old " + brand + "!";
         System.out.println("car is starting....Vvvvroooom " + engineMessage);
+    }
+
+    //methods can also accept parameters/args + same method twice due to method overload..same as constructors
+    public void startRadio(double stationNumber, String stationName){
+        System.out.println("Starting radio...the station number is " + stationNumber + " " + stationName +"!");
+        System.out.println("Enjoy!!!");
+    }
+
+    public void startRadio(double stationNumber){
+        System.out.println("Station number is " + stationNumber);
     }
     
     
@@ -25,6 +35,7 @@ public class Car{
         Car mine = new Car(myCarBrand, myCarAge);
         System.out.println(mine.brand);
         mine.startEngine();
+        mine.startRadio(97.5,"Hit24");
 
     }
 }
