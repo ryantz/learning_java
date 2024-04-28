9f4f2dd5be52cd7a48b84fcb5668750f0aee1b34
ryantz <r.tanzyy@gmail.com>
2024-04-28 11:53:54 +0800

    updated use of this keywords with both variables and methods

--- This_savings.java
+++ This_savings.java
@@ -1,9 +1,11 @@
 public class This_savings{
+    //this keywords can also be used for methods
 
     //declaring instances / gobal variables use priv or pub
     public String owner;
     public int balanceDollar;
     public double balanceEuro;
+    public int password;
 
     public This_savings(String user, int balance){
         //init
@@ -18,10 +20,22 @@ public class This_savings{
         System.out.println("The new balance in your account is: $" + this.balanceDollar);
     }
 
+    public void setPassword(int pass){
+        this.password = pass;
+        System.out.println("The password set is: " + this.password);
+    }
+
+    public void genericPassword(){
+        //using this keywords with methods
+        this.setPassword(12345);
+        System.out.println("The generic password set is: " +  this.password);
+    }
+
     public static void main(String[] args){
         This_savings newAcc = new This_savings("ryan", 300);
         newAcc.addMoney(33);
         System.out.println(newAcc.balanceEuro);
-
+        newAcc.setPassword(975487);
+        newAcc.genericPassword();
     }
 }
