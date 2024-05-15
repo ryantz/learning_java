a5f0bbd63e09fbcb3fea72a6c5f855f3921c544c
ryantz <r.tanzyy@gmail.com>
2024-05-15 20:21:28 +0800

    arrays child classes

---
+++ ChildclassArray.java
@@ -0,0 +1,10 @@
+public class ChildclassArray {
+    public static void main(String[] args){
+        Noodle BiangBiang, Spaetzel;
+        biangbiangz = new BiangBiang();
+        spaetz = new Spaetzel();
+
+        //array of type noodle with biangbiang and spaetzel in order
+        Noodle[] noodletypes = {biangbiangz, spaetz}
+    }
+}
